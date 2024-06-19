c418aa17984034189d6646b57a3d5d640052b3e2
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-06-19 10:11:10 +0200

    reset onscreen controls

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -31,33 +31,33 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveDirection = moveAction.action.ReadValue<Vector2>();
-        transform.Translate(moveDirection * speed * Time.deltaTime);
+        //Vector2 moveDirection = moveAction.action.ReadValue<Vector2>();
+        //transform.Translate(moveDirection * speed * Time.deltaTime);
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxLeft, maxRight), Mathf.Clamp(transform.position.y, maxDown, maxUp), 0);
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxLeft, maxRight), Mathf.Clamp(transform.position.y, maxDown, maxUp), 0);
 
-        //if (Touch.activeTouches.Count  > 0)
-        //{
-        //    if (Touch.activeTouches[0].finger.index == 0)
-        //    {
-        //        Touch myTouch = Touch.activeTouches[0];
+        if (Touch.activeTouches.Count > 0)
+        {
+            if (Touch.activeTouches[0].finger.index == 0)
+            {
+                Touch myTouch = Touch.activeTouches[0];
 
 
-        //        Vector3 touchPos = myTouch.screenPosition;
-        //        touchPos = mainCam.ScreenToWorldPoint(touchPos);
+                Vector3 touchPos = myTouch.screenPosition;
+                touchPos = mainCam.ScreenToWorldPoint(touchPos);
 
-        //        if (Touch.activeTouches[0].phase == TouchPhase.Began)
-        //        {
-        //            offset = touchPos - transform.position;
-        //        }
-        //        if (Touch.activeTouches[0].phase == TouchPhase.Moved || Touch.activeTouches[0].phase == TouchPhase.Stationary)
-        //        {
-        //            transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
-        //        }
-        //    }
+                if (Touch.activeTouches[0].phase == TouchPhase.Began)
+                {
+                    offset = touchPos - transform.position;
+                }
+                if (Touch.activeTouches[0].phase == TouchPhase.Moved || Touch.activeTouches[0].phase == TouchPhase.Stationary)
+                {
+                    transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
+                }
+            }
 
-        //    transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxLeft, maxRight), Mathf.Clamp(transform.position.y, maxDown, maxUp), 0);
-        //}
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxLeft, maxRight), Mathf.Clamp(transform.position.y, maxDown, maxUp), 0);
+        }
     }
 
     private void OnEnable()
