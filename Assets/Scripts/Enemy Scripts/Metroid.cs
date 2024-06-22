9351314433e643ce6a4133a88867989220d1a94b
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-06-22 05:44:20 +0200

    metroid rotation

--- Assets/Scripts/Enemy Scripts/Metroid.cs
+++ Assets/Scripts/Enemy Scripts/Metroid.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 
 public class Metroid : Enemy
 {
-
+    [Header("Main Stats")]
     [SerializeField] private float minSpeed;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private float rotateSpeed;
     private float speed;
 
     void Start()
     {
-        minSpeed = 8;
-        maxSpeed = 15;
+        minSpeed = 6;
+        maxSpeed = 12;
+        rotateSpeed = 80;
         speed = Random.Range(minSpeed, maxSpeed);
 
         rb.velocity = Vector2.down * speed;
@@ -22,7 +24,7 @@ public class Metroid : Enemy
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
     }
 
     public override void HurtSequence()
