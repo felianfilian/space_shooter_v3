cf72f2dee88013ff6462acb360a920e9cce1b236
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-06-22 05:48:53 +0200

    bullet script

---
+++ Assets/Scripts/BulletControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject spawnPosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
