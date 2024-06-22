114dac2c4e9fc21ad623a779b8eb50520667e457
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-06-22 11:53:54 +0200

    shot setup

--- Assets/Scripts/BulletControl.cs
+++ Assets/Scripts/BulletControl.cs
@@ -6,15 +6,23 @@ public class BulletControl : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
     [SerializeField] private GameObject spawnPosition;
+    [SerializeField] private float shotInterval;
+
+    private float shotTimer;
 
     void Start()
     {
-        
+        shotTimer = shotInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        shotTimer -= Time.deltaTime;
+
+        if(shotTimer <= 0)
+        {
+            shotTimer = shotInterval;
+        }
     }
 }
